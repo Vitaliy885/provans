ee372ebbdda056db1b7e3a0127726b1c788e7446
vitalii <vitalik@gmail.com>
2019-01-16 15:30:47 +0200

    kdshjweqhfwf

--- js/common.js
+++ js/common.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
 	$(document).ready(function(){
 		$('.btn-up').mPageScroll2id();
 	});
-	
+	$(".order-za-harakterisikou").click(function(){
+		$(".modal-window-zam-har").css("display","block");
+	});
+
+	$(".zakrutu-modalku").click(function(){
+		$(".modal-window-zam-har").css("display","none");
+	});
+
 });
