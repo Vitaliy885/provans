946f6a4d385f674d29b9d2e5bfeecd0ab3ad4898
vitalii <vitalik@gmail.com>
2019-01-20 03:16:03 +0200

    ghgggf

--- js/common.js
+++ js/common.js
@@ -10,8 +10,29 @@ $(document).ready(function() {
 	$(".zakrutu-modalku").click(function(){
 		$(".modal-window-zam-har").css("display","none");
 	});
-	$(".show-rozmiru").click(function(){
-		$(".rozmiru").slideToggle();
+
+	$(".show-rozmiru-one").click(function(){
+		$(".rozmiru-one").slideToggle();	
+	});
+
+	$(".show-rozmiru-two").click(function(){
+		$(".rozmiru-two").slideToggle();	
+	});
+
+	$(".show-rozmiru-three").click(function(){
+		$(".rozmiru-three").slideToggle();	
+	});
+
+	$(".show-rozmiru-four").click(function(){
+		$(".rozmiru-four").slideToggle();	
+	});
+
+	$(".show-rozmiru-five").click(function(){
+		$(".rozmiru-five").slideToggle();	
+	});
+
+	$(".show-rozmiru-six").click(function(){
+		$(".rozmiru-six").slideToggle();	
 	});
 
 });
