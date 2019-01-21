9932944f52dc917e366271a7a725a1fe585d30bc
vitalii <vitalik@gmail.com>
2019-01-21 02:19:01 +0200

    jbshfcbsdzvcs

--- js/common.js
+++ js/common.js
@@ -21,12 +21,28 @@ $(document).ready(function() {
 		$(".fixed-znujka-modal").css("display","none");
 	});
 
-		$(".zakrutu-modal-blakutna").click(function(){
+	$(".zakrutu-modal-blakutna").click(function(){
 		$(".fixed-blakutna-modal").css("display","none");
 	});
 
-			$(".open-modal-blakutnuy").click(function(){
+	$(".open-modal-blakutnuy").click(function(){
 		$(".fixed-blakutna-modal").css("display","block");
+	});
+
+	$(".zakrutu-modal-bronzova").click(function(){
+		$(".fixed-bronzova-modal").css("display","none");
+	});
+
+	$(".open-modal-bronzova").click(function(){
+		$(".fixed-bronzova-modal").css("display","block");
+	});
+
+		$(".zakrutu-modal-kavova").click(function(){
+		$(".fixed-kavova-modal").css("display","none");
+	});
+
+	$(".open-modal-kavova").click(function(){
+		$(".fixed-kavova-modal").css("display","block");
 	});
 
 
